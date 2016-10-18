c7e95f0baa5974c55047dbcc5031e24b24a36cdd
oarmesto <oarmesto@optimizaclick.com>
2016-10-18 15:55:49 +0200

    2.2.1

--- templates/escaner.php
+++
@@ -1,9 +0,0 @@
-﻿<h2 class="title_migration">Escáner</h2>
-								 
-<table class="form-table centered" id="scan_table">		
-	<thead>
-		<tr><th>Nombre fichero</th><th>Fecha modificación</th><th>Tamaño (bytes)</th><th>Permisos</th></tr>
-	</thead>			
-</table>
-
-<p><input type="button" class="button button-primary" value="Escanear ficheros" id="button_scan_files" /></p>
